a76289f4d132c90deb4f2bbe1ee27a74e185c9e9
saulius <sauliusvaiciulis@yahoo.com>
2023-02-07 10:57:27 +0200

    random word

--- script.js
+++ script.js
@@ -1,45 +1,12 @@
 
-const cookie = document.querySelector("img");
-const points = document.querySelector("h1");
-const buttons = document.querySelectorAll("button")
 
-let counter = 900;
-let number = 1
+const arr = [    "cia",    "yra",    "random",    "zodziai",    "labai",    "nedaug",    "itemu"]
 
-cookie.onclick = () => {
-    counter +=number;
-    points.innerHTML = "Points: " + counter
+const button = document.querySelector("button");
+const word = document.querySelector("h1")
+
+button.onclick = () => {
+    const randomWord = arr[Math.floor(Math.random()*arr.length)]
+    word.innerHTML = "Word: " + randomWord
+    console.log(randomWord)
 }
-
-buttons[0].onclick = () => {
-    if (counter >= 100) {
-        counter -=100;
-        points.innerHTML = "Points: " + counter
-        number = 2
-    }
-}
-buttons[1].onclick = () => {
-    if (counter >= 300) {
-        counter -=300;
-        points.innerHTML = "Points: " + counter
-        number = 3
-    }
-}
-buttons[2].onclick = () => {
-    if (counter >= 500) {
-        counter -=500;
-        points.innerHTML = "Points: " + counter
-        cookie.style.backgroundColor = "red"
-    }
-}
-buttons[3].onclick = () => {
-    if (counter >= 500) {
-        counter -=500;
-        points.innerHTML = "Points: " + counter
-        cookie.src = "https://th.bing.com/th/id/OIP.P0rBWOTEfRzZV3AWoYFuwgHaHa?pid=ImgDet&rs=1"
-    }
-}
-
-
-
-
