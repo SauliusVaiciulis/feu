35f14ef32bdc6c9d7d497cd46683f1d602518980
saulius <sauliusvaiciulis@yahoo.com>
2023-02-07 10:43:23 +0200

    cookie clicker

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
 
+const cookie = document.querySelector("img");
+const points = document.querySelector("h1");
+const buttons = document.querySelectorAll("button")
 
-const buttons = document.querySelectorAll("button");
-const result = document.querySelector("h1");
+let counter = 900;
+let number = 1
 
-let counter = 0;
+cookie.onclick = () => {
+    counter +=number;
+    points.innerHTML = "Points: " + counter
+}
 
 buttons[0].onclick = () => {
-    counter +=10;
-    result.innerHTML = "Counter: " + counter;
+    if (counter >= 100) {
+        counter -=100;
+        points.innerHTML = "Points: " + counter
+        number = 2
+    }
 }
 buttons[1].onclick = () => {
-    counter -=10;
-    result.innerHTML = "Counter: " + counter;
+    if (counter >= 300) {
+        counter -=300;
+        points.innerHTML = "Points: " + counter
+        number = 3
+    }
 }
+buttons[2].onclick = () => {
+    if (counter >= 500) {
+        counter -=500;
+        points.innerHTML = "Points: " + counter
+        cookie.style.backgroundColor = "red"
+    }
+}
+buttons[3].onclick = () => {
+    if (counter >= 500) {
+        counter -=500;
+        points.innerHTML = "Points: " + counter
+        cookie.src = "https://th.bing.com/th/id/OIP.P0rBWOTEfRzZV3AWoYFuwgHaHa?pid=ImgDet&rs=1"
+    }
+}
+
+
+
 
