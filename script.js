b56de5bc2e2cf8efdd13486362aabd5567908f80
saulius <sauliusvaiciulis@yahoo.com>
2023-02-07 09:21:02 +0200

    html and script update

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Some")
+
+
+const boxes = document.querySelectorAll(".box")
+
+boxes[0].onclick = () => {
+    console.log("RED")
+}
+boxes[1].onclick = () => {
+    console.log("AQUA")
+}
+boxes[2].onclick = () => {
+    console.log("VIOLET")
+}
